5c3e09756f9e46787b88d74d06d6f7a2521f5d50
Brook <uedsky@gmail.com>
2024-10-18 12:57:25 +0800

    fix: shadow root portal

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,22 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export function getPortalContainer() {
+  const shadowRoot = document.querySelectorAll('shadow-root-component')
+  let container
+  if(shadowRoot.length) {
+    container = shadowRoot[0].shadowRoot!.lastElementChild
+    // const id = 'shadow-portal-root'
+    // container = target.getElementById(id);
+    // if(!container) {
+    //   container = document.createElement("div");
+    //   container.id = id;
+    //   target.appendChild(container);
+    // }
+  }
+  return container
+}
+
 
 export function getInitials(name: string) {
   if (!name) return '';
